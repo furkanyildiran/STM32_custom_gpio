508fca09fd4acf75fc245fed371cd56a95c90159
furkanyildiran <furkanyldrn0@gmail.com>
2023-03-05 22:09:49 +0300

    rcc register definers added

--- CustomRCC/Inc/custom_rcc_mnemonics.h
+++ CustomRCC/Inc/custom_rcc_mnemonics.h
@@ -51,73 +51,73 @@ extern "C" {
 #define RCC_CR2							(*RCC_CR2_ADDR)
 
 #define _HSION							0x00
-#define _HSI_RDY						0x01
+#define _HSIRDY							0x01
 #define _HSITRIM						0x03
 #define _HSICAL							0x08
-#define _HSE_ON							0x10
-#define _HSE_RDY						0x11
-#define _HSE_BYP						0x12
-#define _CSS_ON							0x13
+#define _HSEON							0x10
+#define _HSERDY							0x11
+#define _HSEBYP							0x12
+#define _CSSON							0x13
 #define _PLLON							0x18
-#define _PLL_RDY						0x19
+#define _PLLRDY							0x19
 
 typedef enum{
 	HSI_OFF,
 	HSI_ON
-}HSI_clock_enable;
+}HSION;
 typedef enum{
 	HSI_not_ready,
 	HSI_ready
-}HSI_clock_ready_flag;
+}HSIRDY;
 typedef enum{
 	HSE_ON,
 	HSE_OFF
-}HSE_clock_enable;
+}HSEON;
 typedef enum{
 	HSE_not_ready,
 	HSE_ready
-}HSE_clock_ready_flag;
+}HSERDY;
 typedef enum{
 	HSE_not_bypassed,
 	HSE_bypassed_with_external_clock
-}HSE_bypass;
+}HSEBYP;
 typedef enum{
 	Clock_security_system_disabled,
 	Clock_security_system_enabled
-}Clock_security_sys_enable;
+}CSSON;
 typedef enum{
 	PLL_OFF,
 	PLL_ON
-}PLL_enable;
+}PLLON;
 typedef enum{
 	PLL_unlocked,
 	PLL_locked
-}PLL_clock_ready_flag;
+}PLLRDY;
 
 #define _SW						0x00
 #define _SWS					0x02
 #define _HPRE					0x04
 #define _PPRE					0x08
-#define _ADC_PRE				0x0E
-#define _PLL_SRC				0x0F
-#define _PLL_XTPRE				0x11
+#define _ADCPRE					0x0E
+#define _PLLSRC					0x0F
+#define _PLLXTPRE				0x11
 #define _PLLMUL					0x12
 #define _MCO					0x18
 #define _MCOPRE					0x1C
-#define _PLL_NODIV				0x1F
+#define _PLLNODIV				0x1F
 
 typedef enum{
 	HSI_selc_sys_clock,
 	HSE_selc_sys_clock,
 	PLL_selc_sys_clock,
 	HSI48_selc_sys_clock
-}System_clock_switch;
+}SW;
 typedef enum{
 	HSI_used_sys_clock,
 	HSE_used_sys_clock,
 	PLL_used_sys_clock,
 	HSI48_used_sys_clock
-}System_clock_switch_status;
+}SWS;
 typedef enum{
 	SYSCLK_not_div = 0b0111,
 	SYSCLK_div2,
@@ -128,20 +128,20 @@ typedef enum{
 	SYSCLK_div128,
 	SYSCLK_div256,
 	SYSCLK_div512
-}HCLK_prescaler;
+}HPRE;
 typedef enum{
 	HCLK_not_div = 0b011,
 	HCLK_div2,
 	HCLK_div4,
 	HCLK_div8,
 	HCLK_Div16
-}PCLK_prescaler;
+}PPRE;
 typedef enum{
 	HSIdiv2_selc_PLL_input_clk,
 	HSIdivPREDIV_selc_PLL_input_clk,
 	HSEdivPREDIV_selc_PLL_input_clk,
 	HSI48divPREDIV_selc_PLL_input_clk,
-}PLL_input_clock_source;
+}PLLSRC;
 typedef enum{
 	PLL_input_clkmul2,
 	PLL_input_clkmul3,
@@ -158,7 +158,7 @@ typedef enum{
 	PLL_input_clkmul14,
 	PLL_input_clkmul15,
 	PLL_input_clkmul16,
-}PLL_mul_factor;
+}PLLMUL;
 typedef enum{
 	MCO_disabled,
 	HSI14_clk_selctd,
@@ -169,7 +169,7 @@ typedef enum{
 	HSE4_32_clk_selctd,
 	PLL_clk_selctd,
 	HSI48_selctd
-}MCU_clk_output;
+}MCO;
 typedef enum{
 	MCO_div1,
 	MCO_div2,
@@ -179,11 +179,124 @@ typedef enum{
 	MCO_div32,
 	MCO_div64,
 	MCO_div128,
-}MCU_clk_outpt_presc;
+}MCOPRE;
 typedef enum{
 	PLL_div2_forMCO,
 	PLL_notdiv_forMCO,
-}PLL_clk_not_div_forMCO;
+}PLLNODIV;
+
+#define _LSIRDYF				0x00
+#define _LSERDYF				0x01
+#define _HSIRDYF				0x02
+#define _HSERDYF				0x03
+#define _PLLRDYF				0x04
+#define _HSI14RDYF				0x05
+#define _HSI48RDYF				0x06
+#define _CSSF					0x07
+#define _LSIRDYIE				0x08
+#define _LSERDYIE				0x09
+#define _HSIRDYIE				0x0A
+#define _HSERDYIE				0x0B
+#define _PLLRDYIE				0x0C
+#define _HSI14RDYIE				0x0D
+#define _HSI48RDYIE				0x0E
+#define _LSIRDYC				0x10
+#define _LSERDYC				0x11
+#define _HSIRDYC				0x12
+#define _HSERDYC				0x13
+#define _PLLRDYC				0x14
+#define _HSI14RDYC				0x15
+#define _HSI48RDYC				0x16
+#define _CSSC					0x17
+
+
+#define _SYSCFGRST				0x00
+#define _USART6RST				0x05
+#define _USART7RST				0x06
+#define _USART7RST				0x07
+#define _ADCRST					0x09
+#define _TIM1RST				0x0B
+#define _SPI1RST				0x0C
+#define _USART1RST				0x0E
+#define _TIM15RST				0x10
+#define _TIM16RST				0x11
+#define _TIM17RST				0x12
+#define DBGMCURST				0x16
+
+#define _TIM2RST				0x00
+#define _TIM3RST				0x01
+#define _TIM6RST				0x04
+#define _TIM7RST				0x05
+#define _TIM14RST				0x08
+#define _WWDGRST				0x0B
+#define _SPI2RST				0x0E
+#define _USART2RST				0x11
+#define _USART3RST				0x12
+#define _USART4RST				0x13
+#define _USART5RST				0x14
+#define _I2C1RST				0x15
+#define _I2C2RST				0x16
+#define _USBRST					0x17
+#define _CANRST					0x19
+#define _CRSRST					0x1B
+#define _PWRRST					0x1C
+#define _DACRST					0x1D
+#define _CECRST					0x1E
+
+#define _DMAEN					0x00
+#define _DAM2EN					0x01
+#define _SRAMEN					0x02
+#define _FLITFEN				0x04
+#define _CRCEN					0x06
+#define _IOPAEN					0x11
+#define _IOPBEN					0x12
+#define _IOPCEN					0x13
+#define _IOPDEN					0x14
+#define _IOPEEN					0x15
+#define _IOPFEN					0x16
+#define _TSCEN					0x18
+
+
+#define _SYSCFGCOMPEN			0x00
+#define _USART6EN				0x05
+#define _USART7EN				0x06
+#define _USART8EN				0x07
+#define _ADCEN					0x09
+#define _TIM1EN					0x0B
+#define _SPI1EN					0x0C
+#define _USART1EN				0x0E
+#define _TIM15EN				0x11
+#define _TIM16EN				0x12
+#define _TIM17EN				0x13
+#define _DBGMCUEN				0x17
+
+
+#define _TIM2EN					0x00
+#define _TIM3EN					0x01
+#define _TIM6EN					0x04
+#define _TIM7EN					0x05
+#define _TIM14EN				0x08
+#define _WWDGEN					0x0B
+#define _SPI2EN					0x0E
+#define _USART2EN				0x11
+#define _USART3EN				0x12
+#define _USART4EN				0x13
+#define _USART5EN				0x14
+#define _I2C1EN					0x15
+#define _I2C2EN					0x16
+#define _USBEN					0x17
+#define _CANEN					0x19
+#define _CRSEN					0x1B
+#define _PWREN					0x1C
+#define _DACEN					0x1D
+#define _CECEN					0x1E
+
+
+#define
+#define _
+#define _
+
+
 #endif
 #ifdef __cplusplus
 }
